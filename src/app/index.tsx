9989e4c350e65f0e8136726a4e2a93b7cd65dd4b
LordCracken <vladislav070201@gmail.com>
2023-02-14 16:17:19 +0300

    add app heading

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+
+const App = () => {
+  return (
+    <Typography variant="h3" component="h1" sx={{ textAlign: 'center', fontSize: { xs: '1.5rem', md: '3rem' } }}>
+      Feature-Driven Todos
+    </Typography>
+  );
+};
+
+export default App;
