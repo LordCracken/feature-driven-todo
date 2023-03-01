5694c3c33db79a4dd13c0b882bc2fbe959675877
LordCracken <vladislav070201@gmail.com>
2023-03-01 16:30:14 +0300

    fix isAuthenticated selector in UserControls component

--- src/features/user/components/UserControls.tsx
+++ src/features/user/components/UserControls.tsx
@@ -1,12 +1,12 @@
 import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from '../../../app/store';
-import { userActions } from '../store/slice';
+import { userActions } from '../store';
 
 import { Button } from '@mui/material';
 
 const UserMenu = () => {
   const dispatch = useAppDispatch();
-  const isAuthenticated = !!useSelector((state: RootState) => state.user.authToken);
+  const isAuthenticated = !!useSelector((state: RootState) => state.user.id);
 
   const openUserModalHandler = () => {
     dispatch(userActions.switchModal());
