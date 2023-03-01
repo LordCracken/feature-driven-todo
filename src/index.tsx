0a3484980e41909b3c43d11aea0eecf69bfa7075
LordCracken <vladislav070201@gmail.com>
2023-03-01 16:06:29 +0300

    add firebase package

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { initializeApp } from 'firebase/app';
 
 import App from './app';
 import { Provider } from 'react-redux';
 import store from './app/store';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDEFS_sKdw0tTLD1-aTYEBQdXKkIgXF7_k',
+  authDomain: 'feature-driven-todos.firebaseapp.com',
+  databaseURL: 'https://feature-driven-todos-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'feature-driven-todos',
+  storageBucket: 'feature-driven-todos.appspot.com',
+  messagingSenderId: '55624396430',
+  appId: '1:55624396430:web:69a3feb043219e270b23df',
+};
+initializeApp(firebaseConfig);
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
