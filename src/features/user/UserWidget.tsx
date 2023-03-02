7455643f0f986661640af0f23f3293c3137e487d
LordCracken <vladislav070201@gmail.com>
2023-03-02 16:52:10 +0300

    simplify autologin & move its logic into UserWidget component

--- src/features/user/UserWidget.tsx
+++ src/features/user/UserWidget.tsx
@@ -1,21 +1,29 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 import UserControls from './components/UserControls';
 import SignInDialog from './components/SignInDialog';
 import SignOutDialog from './components/SignOutDialog';
+
 import { RootState, useAppDispatch } from '../../app/store';
-import { autologinAction } from './store';
+import { userActions } from './store';
+
 import Status from '../../shared/components/Status';
 
 export const UserWidget = () => {
   const dispatch = useAppDispatch();
-  const isAuthenticated = !!useSelector((state: RootState) => state.user.id);
+  const isAuthenticated = useSelector((state: RootState) => state.user.isAuthenticated);
   const status = useSelector((state: RootState) => state.user.status);
   const message = useSelector((state: RootState) => state.user.statusMsg);
 
   useEffect(() => {
-    dispatch(autologinAction());
+    const auth = getAuth();
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        dispatch(userActions.signIn());
+      }
+    });
   }, []);
 
   return (
--- src/features/user/store/actions/index.ts
+++ src/features/user/store/actions/index.ts
@@ -1,4 +1,3 @@
 export * from './signUpAction';
 export * from './signInAction';
 export * from './signOutAction';
-export * from './autologinAction';
