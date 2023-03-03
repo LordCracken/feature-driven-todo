878df346bbf93b7c4ec3adfe6496ab58ca19f789
LordCracken <vladislav070201@gmail.com>
2023-03-03 04:17:19 +0300

    add AuthError,ValueOf and Statuses global types

--- src/features/user/store/actions/interfaces.ts
+++
@@ -1,7 +0,0 @@
-import { AuthErrorCodes } from 'firebase/auth';
-
-type ValueOf<T> = T[keyof T];
-
-export interface AuthError extends Error {
-  code: ValueOf<typeof AuthErrorCodes>;
-}
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,14 @@
 declare module '*.module.scss';
 
+type ValueOf<T> = T[keyof T];
+
 type UniqueID = string;
 type Email = string;
 type Password = string;
+
+declare type Statuses = 'loading' | 'success' | 'error';
+
+declare interface AuthError extends Error {
+  code: ValueOf<typeof import('firebase/auth').AuthErrorCodes>;
+}
+
